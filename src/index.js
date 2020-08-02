9be1ad1e22851face6ce7bfc7ec9c716a784e24c
charitha95 <>
2020-08-02 14:16:13 +0530

    first commit

---
+++ src/index.js
@@ -0,0 +1,26 @@
+// React Required
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+import './index.scss';
+import DarkPortfolioLanding from './dark/PortfolioLanding';
+import error404 from "./elements/error404";
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import * as serviceWorker from './serviceWorker';
+
+
+class Root extends Component {
+    render() {
+        return (
+            <BrowserRouter basename={'/'}>
+                <Switch>
+                    <Route exact path={`${process.env.PUBLIC_URL}/dark-portfolio-landing`} component={DarkPortfolioLanding} />
+                    <Route path={`${process.env.PUBLIC_URL}/404`} component={error404} />
+                    <Route component={error404} />
+                </Switch>
+            </BrowserRouter>
+        )
+    }
+}
+
+ReactDOM.render(<Root />, document.getElementById('root'));
+serviceWorker.register();
